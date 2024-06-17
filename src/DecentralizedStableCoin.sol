cf56c4d2595518e4639d734f6f8843954402fb8e
Herman Effendi <hermaneffendi0502@gmail.com>
2024-06-17 20:32:57 +0700

    first step

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+// Contract elements should be laid out in the following order:
+//     Pragma statements
+//     Import statements
+//     Events
+//     Errors
+//     Interfaces
+//     Libraries
+//     Contracts
+
+// Inside each contract, library or interface, use the following order:
+//     Type declarations
+//     State variables
+//     Events
+//     Errors
+//     Modifiers
+//     Functions
+
+contract A  {}
