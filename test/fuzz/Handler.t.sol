54686b4958009e4213aab33fcdcd3c5652ba562d
Herman Effendi <hermaneffendi0502@gmail.com>
2024-07-08 20:36:31 +0700

    create file invariant

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -6,3 +6,6 @@
  * 1. The total of DSC should be less than the total value of collateral 
  * 2. Getter view functions should never revert
  */
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
